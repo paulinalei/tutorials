1856f3c4a4cc260265a40af7db6814e58df187ea
Paulina Lei <paulina.y.lei@gmail.com>
2017-06-20 06:50:07 +0100

    camera and play area

---
+++ roll a ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        // want camera to be at a certain distance from player at each frame
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+    // LateUpdate is guaranteed to run
+	void LateUpdate () {
+        // update camera so that it will be aligned with the player object even when player moves
+        transform.position = player.transform.position + offset;
+	}
+}
