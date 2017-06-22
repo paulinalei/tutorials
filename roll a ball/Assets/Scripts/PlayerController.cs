3e00cd8f382e37c98ca1addb9e1b03b7269b9d53
Paulina Lei <paulina.y.lei@gmail.com>
2017-06-22 05:41:24 +0100

    collecting the pick up objects

--- roll a ball/Assets/Scripts/PlayerController.cs
+++ roll a ball/Assets/Scripts/PlayerController.cs
@@ -25,5 +25,11 @@ public class PlayerController : MonoBehaviour {
 		float moveVertical = Input.GetAxis ("Vertical");
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rb.AddForce (movement * speed);
-	}		
+	}
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Pick Up")) {
+            other.gameObject.SetActive (false);
+        }
+    }    
 }
