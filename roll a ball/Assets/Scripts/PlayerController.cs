198d47604b8b85e948ce5cb2bf29c16142434b69
Paulina Lei <paulina.y.lei@gmail.com>
2017-06-23 06:19:10 +0100

    displaying the store and text

--- roll a ball/Assets/Scripts/PlayerController.cs
+++ roll a ball/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+    public Text countText;
+    public Text winText;
 
 	private Rigidbody rb;
+    private int count;
 
 	void Start () {
 		rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText ();
+        winText.text = "";
 	}
 
 	// loads before rendering frame
@@ -30,6 +37,15 @@ public class PlayerController : MonoBehaviour {
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Pick Up")) {
             other.gameObject.SetActive (false);
+            count = count + 1; // tutorial didn't use += so just following it
+            SetCountText ();
         }
     }    
+
+    void SetCountText () {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12) {
+            winText.text = "You win!";
+        }
+    }
 }
