cf80ccc7719a7c8dc2190ac56226e744232e2282
Paulina Lei <paulina.y.lei@gmail.com>
2017-06-26 06:37:59 +0100

    setup

---
+++ unity/roll a ball/Assets/Scripts/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    // we want to change rotation values every frame
+    // we want to rotate the transform
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+
+	}
+}
